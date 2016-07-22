ce29d4fe1f3db6f70054e6a3fcd97541c891027f
Tony Edwards <tony@plymouthsoftware.com>
2016-07-22 04:00:46 +0100

    Launch ready

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+/* jshint latedef:nofunc */
+
+var app = {
+  container: 0,
+  containerWidth: 0,
+  triggerHeight: 0,
+  h3Location: null,
+  h3Supporters: 0,
+  h3Newsletter: 0,
+  lastScroll: 0,
+  scrollingDown: null
+};
+
+(function() {
+  setupApp();
+  setScrollDirection();
+  enlargeH3Underlines();
+})();
+
+function setScrollDirection() {
+  window.addEventListener('scroll', function(){
+    var curPos = window.pageYOffset || document.body.scrollTop;
+
+    app.scrollingDown = curPos > app.lastScroll ? true : false;
+    app.lastScroll = curPos;
+    // console.log(app.scrollingDown);
+  });
+}
+
+function enlargeH3Underlines() {
+  window.addEventListener('scroll', function(){
+    var locationDistanceToTop = app.h3Location.getBoundingClientRect().top;
+    if (locationDistanceToTop <= app.triggerHeight && locationDistanceToTop > 0){
+      app.h3Location.classList.add('animated-h3-border');
+    }
+
+    var supportersDistanceToTop = app.h3Supporters.getBoundingClientRect().top;
+    if (supportersDistanceToTop <= app.triggerHeight && supportersDistanceToTop > 0){
+      app.h3Supporters.classList.add('animated-h3-border');
+    }
+
+    var newsletterDistanceToTop = app.h3Newsletter.getBoundingClientRect().top;
+    if (newsletterDistanceToTop <= app.triggerHeight && newsletterDistanceToTop > 0){
+      app.h3Newsletter.classList.add('animated-h3-border');
+    }
+  });
+}
+
+// function locationH3() {
+//   console.log('locationh3');
+//   var triggerBreakdown = app.triggerHeight / 100;
+//   var remainingH3Width = app.containerWidth - app.h3Location.offsetWidth;
+//   var remainingBreakdown = remainingH3Width / 100;
+// }
+
+function setupApp() {
+  // Store reference to reused items
+  // app.container = document.getElementsByClassName('container')[0]; //.clientWidth;
+  app.h3Location = document.getElementById('location'); //.offsetWidth;
+  app.h3Supporters = document.getElementById('supporters'); //.offsetWidth;
+  app.h3Newsletter = document.getElementById('newsletter'); //.offsetWidth;
+
+  // Get widths and current positions
+  // app.containerWidth = calculateContainerWidth(app.container);
+  app.lastScroll = window.pageYOffset || document.body.scrollTop;
+
+  // Calculate the distance from top that h3 effects should trigger
+  app.triggerHeight = Math.floor(window.innerHeight - ((window.innerHeight / 100) * 10));
+
+  console.log(app);
+}
+
+// function calculateContainerWidth(container) {
+//   // Why on earth is getting the padded value of a container so convoluted?
+//   var style = window.getComputedStyle(container, null);
+//   var padding = style.getPropertyValue('padding-left');
+//   padding = padding.substr(0, padding.length-2) * 2;
+//   return container.clientWidth - padding;
+// }
